bce1bd490930a6852ab9237af6661636e5c19497
Jim Keener <jim@jimkeener.com>
2015-10-13 00:23:39 -0400

    adds a useful list of primes

--- smallbloom.c
+++ smallbloom.c
@@ -10,7 +10,14 @@ uint8_t *filter;
  */
 #define LFSR(c) ((( (c) << 1) ^ (((c) & 0x80) ? 0x1d : 0)) % 256)
 #define FILTER_LENGTH_BYTES 14
-// Should be a prime
+/* Should be a prime
+ *   2      3      5      7     11     13     17     19     23     29
+ *  31     37     41     43     47     53     59     61     67     71
+ *  73     79     83     89     97    101    103    107    109    113
+ * 127    131    137    139    149    151    157    163    167    173
+ * 179    181    191    193    197    199    211    223    227    229
+ * 233    239    241    251
+ */
 #define FILTER_LENGTH_BITS 113 //8*FILTER_LENGTH_BYTES
 
 #define BF_ITER 4
